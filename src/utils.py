7ae240f3b1929f32c7af18ecd391eff1cd9dcde8
jbgriesner <jean.benoit.griesner@gmail.com>
2020-05-21 15:20:41 +0200

    clean

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
@@ -57,3 +59,52 @@ def train_tune_test_split(clean_DIR, X, test_min_clicks):
                 for item in items:
                     train.write(f"{user}\t{item}\n")
             user += 1
+
+
+def get_count(tp, id):
+    playcount_groupbyid = tp[[id]].groupby(id, as_index=False)
+    count = playcount_groupbyid.size()
+    return count
+
+def filter_rows(tp, min_uc=5, min_sc=5):
+    # Only keep the triplets for items which were clicked on by at least min_sc users.
+    if min_sc > 0:
+        itemcount = get_count(tp, 'item')
+        tp = tp[tp['item'].isin(itemcount.index[itemcount >= min_sc])]
+
+    # Only keep the triplets for users who clicked on at least min_uc items
+    # After doing this, some of the items will have less than min_uc users, but should only be a small proportion
+    if min_uc > 0:
+        usercount = get_count(tp, 'user')
+        tp = tp[tp['user'].isin(usercount.index[usercount >= min_uc])]
+
+    # Update both usercount and itemcount after filtering
+    usercount, itemcount = get_count(tp, 'user'), get_count(tp, 'item')
+    return tp, usercount, itemcount
+
+def split_train_test_proportion(data, test_prop=0.2):
+    data_grouped_by_user = data.groupby('user')
+    tr_list, te_list = list(), list()
+
+    np.random.seed(98765)
+
+    for i, (_, group) in enumerate(data_grouped_by_user):
+        n_items_u = len(group)
+
+        if n_items_u >= 5:
+            idx = np.zeros(n_items_u, dtype='bool')
+            idx[np.random.choice(n_items_u, size=int(test_prop * n_items_u), replace=False).astype('int64')] = True
+
+            tr_list.append(group[np.logical_not(idx)])
+            te_list.append(group[idx])
+        else:
+            tr_list.append(group)
+
+        if i % 1000 == 0:
+            print("%d users sampled" % i)
+            sys.stdout.flush()
+
+    data_tr = pd.concat(tr_list)
+    data_te = pd.concat(te_list)
+
+    return data_tr, data_te
